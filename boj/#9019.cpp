f2390baa57810bd5a1a201ab045114ab93d81fe8
JDhyeok <jdhyeok97@gmail.com>
2021-08-08 16:03:05 +0900

    boj solution

--- boj/#9019.cpp
+++ boj/#9019.cpp
@@ -4,6 +4,11 @@
 
 using namespace std;
 
+/*
+memory : 2288 KB
+time : 3960 ms
+*/
+
 bool visited[10000] = { false, };
 
 int changeNum(char order, int num) {
@@ -100,4 +105,100 @@ int main() {
     }
 
     return 0;
+}
+
+/*********************** 다른 풀이 ***********************/
+/*
+memory : 2456 KB
+time : 2832 ms
+*/
+
+int visited[10000] = { false, };
+int q[100000] = { 0, };
+int i = 0, j = 0;
+int origin, dest;
+
+int change(char order, int n) {
+	int tmp = 0;
+	switch (order) {
+	case 'D':
+		return (n * 2) % 10000;
+	case 'S':
+		if (n == 0)
+			return 9999;
+		return n - 1;
+	case 'L':
+		tmp = n / 1000;
+		n = (n % 1000) * 10;
+		return (n + tmp);
+	case 'R':
+		tmp = n % 10;
+		n = n / 10;
+		return (n + (tmp * 1000));
+	}
+}
+
+string bfs() {
+	queue<string> qOrder;
+
+	qOrder.push("");
+	q[j++] = origin;
+
+	while (i < j) {
+		int cur = q[i++];
+		string curOrder = qOrder.front();
+		qOrder.pop();
+		int Dn = change('D', cur);
+		if (Dn == dest)
+			return curOrder + 'D';
+		if (!visited[Dn]) {
+			visited[Dn] = true;
+			q[j++] = Dn;
+			qOrder.push(curOrder + 'D');
+		}
+
+		int Sn = change('S', cur);
+		if (Sn == dest)
+			return curOrder + 'S';
+		if (!visited[Sn]) {
+			visited[Sn] = true;
+			q[j++] = Sn;
+			qOrder.push(curOrder + 'S');
+		}
+
+		int Ln = change('L', cur);
+		if (Ln == dest)
+			return curOrder + 'L';
+		if (!visited[Ln]) {
+			visited[Ln] = true;
+			q[j++] = Ln;
+			qOrder.push(curOrder + 'L');
+		}
+
+		int Rn = change('R', cur);
+		if (Rn == dest)
+			return curOrder + 'R';
+		if (!visited[Rn]) {
+			visited[Rn] = true;
+			q[j++] = Rn;
+			qOrder.push(curOrder + 'R');
+		}
+	}
+	return "";
+}
+
+int main() {
+
+	int T;
+
+	cin >> T;
+	for (int t = 0; t < T; t++) {
+		cin >> origin >> dest;
+		i = 0; j = 0;
+		fill_n(visited, 10000, false);
+		fill_n(q, 100000, 0);
+		cout << bfs() << "\n";
+	}
+
+	return 0;
 }
