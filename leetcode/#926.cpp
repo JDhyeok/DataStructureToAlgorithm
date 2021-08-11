b0de30f5057fa7bc24807c182c9f45878b447297
JDhyeok <jdhyeok97@gmail.com>
2021-08-11 16:05:17 +0900

    leetcode solution

--- leetcode/#926.cpp
+++ leetcode/#926.cpp
@@ -25,3 +25,22 @@ public:
         return ans;
     }
 };
+
+// More Simple
+class Solution {
+public:
+    int minFlipsMonoIncr(string s) {
+        int counter_one = 0;
+        int counter_flip = 0;
+        
+        for (auto ch : s) {
+            if (ch == '1') {
+                ++counter_one;
+            } else {
+                ++counter_flip;
+            }
+            counter_flip = smin(counter_one, counter_flip);
+        }
+        return counter_flip;
+    }
+};
