64798b22d48d7e8195892d5c2e59b59b117ffb1f
JDhyeok <jdhyeok97@gmail.com>
2021-08-16 11:00:04 +0900

    boj solution

--- boj/#11053.cpp
+++ boj/#11053.cpp
@@ -33,5 +33,33 @@ int main() {
 	
 	cout << ans << "\n";
 
+	// 이분탐색을 이용한 풀이
+	/*
+	int N, ans = 0;
+	int A[1002] = { 0, }, dp[1002] = { 0, };
+
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		cin >> A[i];
+
+	vector<int> vec;
+
+	for (int i = 0; i < N; i++) {
+		if (vec.empty()) {
+			vec.push_back(A[i]);
+		}
+		else {
+			if (vec.back() < A[i])
+				vec.push_back(A[i]);
+			else {
+				auto iter = lower_bound(vec.begin(), vec.end(), A[i]);
+				*iter = A[i];
+			}
+		}
+	}
+
+	cout << vec.size() << "\n";
+	*/
+
 	return 0;
 }
