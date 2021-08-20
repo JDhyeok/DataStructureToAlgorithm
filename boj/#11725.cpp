8fe8a6282a592ab49516a979cf00fb067ab81305
JDhyeok <jdhyeok97@gmail.com>
2021-08-20 11:01:09 +0900

    boj solution

--- boj/#11725.cpp
+++ boj/#11725.cpp
@@ -5,6 +5,10 @@
 
 using namespace std;
 
+
+// bfs
+// time : 136ms
+// memorty : 11700 KB
 int main() {
 
 	cin.tie(NULL);
@@ -42,6 +46,53 @@ int main() {
 	for (int i = 2; i <= N; i++) {
 		cout << root[i] << "\n";
 	}
+
+	return 0;
+}
+
+// dfs
+// time : 48ms
+// memorty : 12584 KB
+int N;
+bool visited[100001];
+int parent[100001];
+vector<int> tree[100001];
+
+void dfs(int root) {
+
+	visited[root] = true;
+
+	for (int i = 0; i < tree[root].size(); i++) {
+		if (visited[tree[root][i]])
+			continue;
+
+		visited[tree[root][i]] = true;
+		parent[tree[root][i]] = root;
+		dfs(tree[root][i]);
+		
+	}
+}
+
+int main() {
+
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N;
+
+	for (int i = 1; i < N; i++) {
+		int a, b;
+		cin >> a >> b;
+
+		tree[a].push_back(b);
+		tree[b].push_back(a);
+	}
+
+	dfs(1);
+
+
+	for (int i = 2; i <= N; i++)
+		cout << parent[i] << "\n";
 
 	return 0;
 }
