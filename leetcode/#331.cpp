f03d19f12e79428298046c601932d7d577e8f2c3
JDhyeok <jdhyeok97@gmail.com>
2021-08-26 21:16:24 +0900

    leetcode solution

---
+++ leetcode/#331.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool isValidSerialization(string preorder) {
+        if(preorder.size() == 1 && preorder[0] == '#')
+            return true;
+        
+
+        stack<pair<int, int>> s;
+        string tmp;
+        int idx = 0;
+
+        for (int i = 0; i < preorder.size(); i++) {
+            if ('0' <= preorder[i] && preorder[i] <= '9') {
+                tmp = "";
+                while ('0' <= preorder[i] && preorder[i] <= '9') {
+                    tmp += preorder[i];
+                    i++;
+                }
+                if (!s.empty()) {
+                    s.top().second--;
+
+                    if (s.top().second == 0)
+                        s.pop();
+                }
+                s.push(make_pair(stoi(tmp), 2));
+
+            }
+            else if (preorder[i] == '#') {
+                if (s.empty())
+                    return false;
+                s.top().second--;
+                if (s.top().second == 0)
+                    s.pop();
+                
+                if(s.empty() && i < preorder.size() - 2)
+                    return false;
+            }
+            else
+                continue;
+        }
+
+        return !s.size();
+    }
+};
