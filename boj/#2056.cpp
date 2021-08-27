8105bbfbf95c27cb32bdf755ee39a5eb451c4e24
JDhyeok <jdhyeok97@gmail.com>
2021-08-27 09:50:40 +0900

    boj solution

---
+++ boj/#2056.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int N;
+vector<int> vec[10001];
+vector<int> times;
+int dp[10001] = { 0, };
+
+int dfs(int init) {
+
+	if (dp[init] != 0)
+		return dp[init];
+
+	int t = 0;
+
+	for (int i = 0; i < vec[init].size(); i++) {
+		t = max(t, dfs(vec[init][i]));
+	}
+	dp[init] = t + times[init - 1];
+	return t + times[init-1];
+}
+
+int main() {
+
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	int time, num, node;
+	vector<int> init;
+
+	cin >> N;
+	
+	for (int i = 1; i <= N; i++) {
+		cin >> time >> num;
+		if (num == 0) {
+			init.push_back(i);
+		}
+		times.push_back(time);
+		for (int j = 0; j < num; j++) {
+			cin >> node;
+			vec[i].push_back(node);
+		}
+	}
+
+	int ans = 0;
+
+	for (int i = 1; i <= N; i++) {
+		ans = max(ans, dfs(i));
+	}
+
+	cout << ans;
+
+	return 0;
+}
