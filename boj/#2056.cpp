2495aec4949cb24c4d2fd54670e1d491d9ed9982
JDhyeok <jdhyeok97@gmail.com>
2021-08-27 10:30:27 +0900

    boj solution

--- boj/#2056.cpp
+++ boj/#2056.cpp
@@ -55,3 +55,29 @@ int main() {
 
 	return 0;
 }
+
+// O(N) 풀이
+int main() {
+
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	int N;
+	cin >> N;
+	int ans = 0;
+	array<int, 10001> work;
+
+	for (int i = 1; i <= N; i++) {
+		int t, pre, cost = 0;
+		cin >> t >> pre;
+		for (int j = 0; j < pre; j++) {
+			int p;
+			cin >> p;
+			cost = max(cost, work[p]);
+		}
+		work[i] = t + cost;
+		ans = max(ans, work[i]);
+	}
+	cout << ans;
+	return 0;
+}
