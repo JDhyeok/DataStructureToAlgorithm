e049f09aece333ccf46c0d67477e7f91d1d81817
JDhyeok <jdhyeok97@gmail.com>
2021-09-28 11:43:13 +0900

    boj solution

--- boj/#11054.cpp
+++ boj/#11054.cpp
@@ -61,3 +61,49 @@ int main() {
 
 	return 0;
 }
+
+
+// Optimization Solution
+#define MAX 1001
+
+int N;
+int A[MAX], dpRight[MAX], dpLeft[MAX];
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin >> N;
+	
+	for (int i = 0; i < N; i++)
+		cin >> A[i];
+
+	fill_n(dpRight, MAX, 1);
+	fill_n(dpLeft, MAX, 1);
+
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < i; j++) {
+			if (A[i] > A[j])
+				dpRight[i] = max(dpRight[i], dpRight[j] + 1);
+		}
+	}
+
+	for (int i = N - 1; i >= 0; i--) {
+		for (int j = N - 1; j > i; j--) {
+			if (A[i] > A[j])
+				dpLeft[i] = max(dpLeft[i], dpLeft[j] + 1);
+		}
+	}
+
+	int ans = 0;
+
+	for (int i = 0; i < N; i++)
+		ans = max(ans, dpRight[i] + dpLeft[i]);
+
+	cout << ans - 1 << "\n";
+
+	return 0;
+}
