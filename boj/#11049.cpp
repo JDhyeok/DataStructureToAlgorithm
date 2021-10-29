b4b1d1ec3448d4466fa5f75b875239f49c283d76
JDhyeok <jdhyeok97@gmail.com>
2021-10-29 22:57:19 +0900

    boj solution

--- boj/#11049.cpp
+++ boj/#11049.cpp
@@ -13,14 +13,6 @@ int N;
 int dp[500][500];
 vector<Matrix> mat;
 
-int calcCount(Matrix& A, Matrix& B) {
-	return (A.r * A.c * B.c);
-}
-
-Matrix multMat(Matrix& A, Matrix& B) {
-	return Matrix(A.r, B.c);
-}
-
 int solve(int s, int e) {
 	// base case
 	if (s == e)
@@ -64,5 +56,41 @@ int main() {
 
 	cout << dp[0][mat.size() - 1];
 
+	return 0;
+}
+
+
+// O(N^3)의 더 나은 풀이
+
+int N;
+int mat[501][2], dp[501][501];
+
+
+int main() {
+	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	cin >> N;
+
+	for (int i = 1; i <= N; i++)
+		cin >> mat[i][0] >> mat[i][1];
+
+	for (int i = 0; i < N; i++)
+		dp[i][i + 1] = mat[i][0] * mat[i][1] * mat[i + 1][1];
+
+	for (int m = 2; m <= N; m++) {				// 길이
+		for (int i = 1; i <= N - m; i++) {		// 시작 위치
+			int j = i + m;						// 끝 위치
+			dp[i][j] = 987654321;
+			for (int k = i; k < j; k++) {
+				dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j] + (mat[i][0] * mat[k][1] * mat[j][1]));
+			}
+		}
+	}
+
+	cout << dp[1][N];
 	return 0;
 }
