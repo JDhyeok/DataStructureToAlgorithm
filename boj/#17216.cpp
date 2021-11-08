2969816a623e0f39b021eb21551ae23b9d8e068c
JDhyeok <jdhyeok97@gmail.com>
2021-11-08 10:28:48 +0900

    boj solution

---
+++ boj/#17216.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+	 
+using namespace std;
+	
+#define MAX 1001
+	
+int N, A[MAX];
+int dp[MAX];
+
+int main(){
+	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin >> N;
+	
+	for(int i = 0; i < N; i++){
+		cin >> A[i];
+	}
+	
+	dp[0] = A[0];
+	int ans = -1;
+	
+	for(int i = 1; i < N; i++) {
+		dp[i] = A[i];
+		for(int j = 0; j < i; j++){
+			if(A[j] > A[i]){
+				dp[i] = max(dp[i], A[i] + dp[j]);
+			}
+		}
+		ans = max(ans, dp[i]);
+	}
+	
+	cout << ans;
+	
+	return 0;
+}
