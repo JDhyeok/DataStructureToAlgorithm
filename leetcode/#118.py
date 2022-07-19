7456c0e6fa7b73f8c8ae0bdb85bdb71416eed82b
JDhyeok <jdhyeok97@gmail.com>
2022-07-19 23:25:41 +0900

    leetcode solution

---
+++ leetcode/#118.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        ans = []
+        
+        for i in range(numRows):
+            tmp = []
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    tmp.append(1)
+                else:
+                    tmp.append(ans[i-1][j] + ans[i-1][j-1])
+            ans.append(tmp)
+            
+        return ans
