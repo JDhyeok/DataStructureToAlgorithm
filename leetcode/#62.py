31f21fd4665fb285402b46258173e3c581f71f4a
JDhyeok <jdhyeok97@gmail.com>
2022-08-01 23:04:10 +0900

    leetcode solution

--- leetcode/#62.py
+++ leetcode/#62.py
@@ -5,13 +5,23 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp = [[0]*n for _ in range(m)]
+        # 44ms / 13.3 MB
+        # dp = [[0]*n for _ in range(m)]
         
-        for i in range(m):
-            for j in range(n):
-                if i == 0 or j == 0:
-                    dp[i][j] = 1
-                else:
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        # for i in range(m):
+        #     for j in range(n):
+        #         if i == 0 or j == 0:
+        #             dp[i][j] = 1
+        #         else:
+        #             dp[i][j] = dp[i-1][j] + dp[i][j-1]
         
-        return dp[m-1][n-1]
+        # return dp[m-1][n-1]
+        
+        # 37ms / 13.3MB
+        dp = [1]*n
+        
+        for _ in range(m - 1):
+            for i in range(1, n):
+                dp[i] = dp[i] + dp[i - 1]
+
+        return dp[-1]
